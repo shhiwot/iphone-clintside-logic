72433221137f03bbb6bb5fe45256adc84cfff7f5
shhiwot <shhiwot14@gmail.com>
2024-08-15 09:48:44 -0500

    update

--- src/Classbased_component/Section/Youtube/Youtube.jsx
+++ src/Classbased_component/Section/Youtube/Youtube.jsx
@@ -16,7 +16,8 @@ export default function Youtube() {
   const [Youtubedata, setYoutubedata] = useState([]);
   useEffect(() => {
     fetch(
-        "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDyXH_nhx3jJViVzBrRNaf6-T54cFQwiBk&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet,id&order=date&maxResults=8"
+      "../../../../appledata.json"
+        // "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDyXH_nhx3jJViVzBrRNaf6-T54cFQwiBk&channelId=UCE_M8A5yxnLfW0KghEeajjw&part=snippet,id&order=date&maxResults=8"
     )
       .then((res) => res.json())
       .then((data) => {
